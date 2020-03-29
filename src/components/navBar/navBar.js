f2106d93c4a74508e4db66eee3c81c14c75560a5
James Cockbain <james.cockbain@ibm.com>
2020-03-29 19:32:55 +0100

    add simple routing object (#12)

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -11,10 +11,10 @@ const navBar = () => (
     <Header>
       <Logo />
       <div>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
           <Menu.Item key="1">Arrays</Menu.Item>
-          <Menu.Item key="2">Graphs</Menu.Item>
-          <Menu.Item key="3">Trees</Menu.Item>
+          {/* <Menu.Item key="2">Graphs</Menu.Item>
+          <Menu.Item key="3">Trees</Menu.Item> */}
         </Menu>
       </div>
     </Header>
---
+++ src/containers/comingSoon/comingSoon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Result } from 'antd';
+
+const ComingSoon = () => (
+  <Result
+    status="info"
+    title="Coming Soon!"
+    subTitle="This page hasn't been made yet, stay tuned."
+  />
+);
+
+export default ComingSoon;
