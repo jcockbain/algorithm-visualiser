b5ee123b1c9c1a1ebeb53bd3db569d417e9eb311
James Cockbain <james.cockbain@ibm.com>
2020-03-30 23:23:37 +0100

    add control of sorting speed (#15)

--- src/containers/BubbleSort/BubbleSort.js
+++ src/containers/BubbleSort/BubbleSort.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import {
-  Button, Row, Col, InputNumber,
+  Button, Row, Col, InputNumber, Slider,
 } from 'antd';
 import { shuffle } from 'lodash';
 import {
@@ -16,9 +16,8 @@ const CardContainer = styled.div`
   margin-top: 1rem;
 `;
 
-const StyledButton = styled(Button)`
-  /* display: block;
-  margin: auto; */
+const SliderWrapper = styled.div`
+  padding-top: 0.2rem;
 `;
 
 const CenteredDiv = styled.div`
@@ -28,12 +27,13 @@ const CenteredDiv = styled.div`
 
 const initialCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const AlgorithmContainer = () => {
+const BubbleSort = () => {
   const [cards, setCards] = useState(initialCards);
   const [sortIndex, setSortIndex] = useState(0);
   const [runSort, setRunSort] = useState(false);
   const [swapped, setSwapped] = useState(false);
   const [sorted, setSorted] = useState(false);
+  const [sortSpeed, setSortSpeed] = useState(5);
 
   const updateCardLength = (value) => {
     const newArray = [];
@@ -85,47 +85,48 @@ const AlgorithmContainer = () => {
 
   useEffect(() => {
     if (runSort) {
-      setTimeout(() => bubbleSort(), 500);
+      setTimeout(() => bubbleSort(), 1600 - 150 * (sortSpeed));
     }
   }, [runSort, cards]);
 
+  const cardsToHighlight = sorted ? cards + [0] : [sortIndex, sortIndex + 1];
   const cardsLength = (cards) ? cards.length : 0;
   return (
     <div>
-      <Row justify="flex-start" gutter={[16, 8]}>
+      <Row justify="left" gutter={[16, 8]}>
         <Col span={4}>
-          <StyledButton
+          <Button
             disabled={runSort}
             icon={<ReloadOutlined />}
             onClick={shuffleCards}
           >
             Shuffle
-          </StyledButton>
+          </Button>
         </Col>
         <Col span={4}>
-          <StyledButton
+          <Button
             disabled={runSort}
             icon={<StepForwardOutlined />}
             onClick={() => bubbleSort()}
           >
             Next Sort
-          </StyledButton>
+          </Button>
         </Col>
         <Col span={4}>
           {runSort ? (
-            <StyledButton
+            <Button
               icon={<PauseOutlined />}
               onClick={() => setRunSort(false)}
             >
               Stop Sort
-            </StyledButton>
+            </Button>
           ) : (
-            <StyledButton
+            <Button
               icon={<CaretRightOutlined />}
               onClick={() => setRunSort(true)}
             >
               Run Sort
-            </StyledButton>
+            </Button>
           )}
         </Col>
       </Row>
@@ -142,9 +143,22 @@ const AlgorithmContainer = () => {
             />
           </CenteredDiv>
         </Col>
+        <Col span={4}>
+          <CenteredDiv>
+            <p>Sort Speed:</p>
+            <SliderWrapper>
+              <Slider
+                min={1}
+                max={10}
+                onChange={(value) => setSortSpeed(value)}
+              />
+            </SliderWrapper>
+          </CenteredDiv>
+        </Col>
       </Row>
+      <Row justify="left" gutter={[16, 16]} />
       <CardContainer>
-        <CardBox cards={cards} selectedCards={[sortIndex, sortIndex + 1]} />
+        <CardBox cards={cards} selectedCards={cardsToHighlight} />
       </CardContainer>
       <Description AlgorithmInfo={algorithmInfo.bubbleSort} />
     </div>
@@ -152,4 +166,4 @@ const AlgorithmContainer = () => {
 };
 
 
-export default AlgorithmContainer;
+export default BubbleSort;
